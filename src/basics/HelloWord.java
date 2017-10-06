595c19a10f28d5a2b3969897058e59b361341fb6
Diogo <diogo-castro@live.com.pt>
2017-10-06 20:22:06 +0100

    Modified Hello Word app

--- src/basics/HelloWord.java
+++ src/basics/HelloWord.java
@@ -3,5 +3,6 @@ package basics;
 public class HelloWord {
 	public static void main(String[] args){
 		System.out.println("Hello World");
+		System.out.println("I'm adding code to my project");
 	}
 }
