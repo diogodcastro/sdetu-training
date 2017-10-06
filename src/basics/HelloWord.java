0ab087cf6c946c0cbb9ea601faf15309a07ff335
diogodcastro <32579724+diogodcastro@users.noreply.github.com>
2017-10-06 20:24:28 +0100

    Update HelloWord.java

--- src/basics/HelloWord.java
+++ src/basics/HelloWord.java
@@ -4,5 +4,6 @@ public class HelloWord {
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("I'm adding code to my project");
+		System.out.println("I'm adding code from gitHub");
 	}
 }
